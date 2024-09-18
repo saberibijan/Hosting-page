1659fa05fd1444d220ffcbae070bfa2e725bbb66
Saberi Bijan <bsaberi93@gmail.com>
2024-09-18 10:48:45 +0330

    add feature

--- scripts/app.js
+++ scripts/app.js
@@ -51,3 +51,17 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// ----------------------------------------------------------------------------
+
+
+
+const toTop = document.querySelector('.to-top');
+
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset > 400){
+        toTop.classList.add('active');
+    }else{
+        toTop.classList.remove('active');
+    }
+})
